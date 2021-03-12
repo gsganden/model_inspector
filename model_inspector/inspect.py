4701ba0f3f7b4c21a68fe6aed4260ab5c9a0c1dd
gsganden <gsganden@gmail.com>
2021-03-11 22:17:20 -0600

    Have linear model HTML func take params rather than model, simplify logistic HTML func

--- model_inspector/inspect.py
+++ model_inspector/inspect.py
@@ -16,7 +16,8 @@ import waterfall_chart
 
 # Cell
 def generate_linear_model_html(
-    model,
+    intercept: float,
+    coefs: Sequence[float],
     feature_names: Iterable[str],
     target_name: str,
     intercept_formatter: str = ".2f",
@@ -31,21 +32,21 @@ def generate_linear_model_html(
     - features: blue
 
     Parameters:
-    - `model`: fitted scikit-learn linear model of the form
-    `y = b0 + b1 * x1 + ...`.
+    - `intercept`: intercept value
+    - `coefs`: coefficient values
     - `feature_names`: feature names in the order in which they were
     given to the model.
     - `target_name` : Name of target variable `y`
     - `intercept_formatter`: Format specifier for model intercept
     - `coef_formatter`: Format specifier for model coefficients
     """
-    if len(model.coef_) != len(feature_names):
-        raise ValueError("len(model.coef_) != len(feature_cols)")
+    if len(coefs) != len(feature_names):
+        raise ValueError("len(coefs) != len(feature_cols)")
     model_string = f"""
         <span style='color:red'>{target_name}</span>
-        = <span style='color:purple'>{model.intercept_:{intercept_formatter}}</span>
+        = <span style='color:purple'>{intercept:{intercept_formatter}}</span>
     """
-    for coef, feature_col in zip(model.coef_, feature_names):
+    for coef, feature_col in zip(coefs, feature_names):
         model_string += f"""
             <span style='color:green'>{"+" if coef >= 0 else "-"} {abs(coef):{coef_formatter}}</span>
             * <span style='color:blue'>{feature_col}</span>
@@ -78,22 +79,16 @@ def generate_logistic_model_html(
     - `intercept_formatter`: Format specifier for model intercept
     - `coef_formatter`: Format specifier for model coefficients
     """
-    for coefs in model.coef_:
-        if len(coefs) != len(feature_names):
-            raise ValueError("len(model.coef_) != len(feature_cols)")
     model_string = "<p>"
     for target_name, coefs, intercept in zip(
         target_val_names, model.coef_, model.intercept_
     ):
-        model_string += f"""
-            <span style='color:red'>log-odds({target_name})</span>
-            = <span style='color:purple'>{intercept:{intercept_formatter}}</span>
-        """
-        for coef, feature_col in zip(coefs, feature_names):
-            model_string += f"""
-                <span style='color:green'>{"+" if coef >= 0 else "-"} {abs(coef):{coef_formatter}}</span>
-                * <span style='color:blue'>{feature_col}</span>
-            """
+        model_string += generate_linear_model_html(
+            intercept=intercept,
+            coefs=coefs,
+            feature_names=X_iris.columns,
+            target_name=f"log-odds({targ_name})",
+        )
         model_string += "</p>"
     return model_string
 
